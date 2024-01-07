c2fc908fcae9f5e7aa8ef33c41305c5d92eca1db
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-07 20:32:49 +0200

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,27 +3,29 @@
 import signUpUser from "./4-user-promise.js";
 import uploadPhoto from "./5-photo-reject.js";
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName)  {
+  try {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
 
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then(([userResult, photoResult]) => {
-      return [
+    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
+
+    return [
       {
         status: userResult.status,
         value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason,
       },
       {
         status: photoResult.status,
-        value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason,
+        value: photoResult.status === 'fulfilled' ? photoResult.value : `${photoResult.reason}`,
       },
     ];
-    })
-    .catch((error) => {
-      return [
-        {
+  } catch (error) {
+    return [
+      {
         status: 'rejected',
-        value: error.message,
+        value: `Error: ${error.message}`,
       },
-      ];
-    });
+    ];
+  }
 }
