ec58dceff304edbc801e71b97bebb70386da5ba3
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-07 20:47:06 +0200

    task 8

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function divideFunction(numerator, denominator) {
+  try {
+    if (denominator === 0)
+      throw Error('cannot divide by 0');
+    return numerator/denominator;
+  } catch (error) {
+    return error;
+  }
+}
