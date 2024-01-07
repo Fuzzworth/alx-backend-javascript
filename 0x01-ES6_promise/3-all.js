4c801922352667e7da5ce653c3dab603a8e7f26c
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-07 15:51:00 +0200

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,17 +2,17 @@
 /* eslint-disable */
 import {uploadPhoto, createUser } from "./utils.js";
 export default function handleProfileSignup(){
-  
+
   uploadPhoto().then((result) => {
-    process.stdout.write(`${result.body} `);
+    createUser().then((result2) => {
+      process.stdout.write(`${result.body} ${result2.firstName} ${result2.lastName}`);
+    }).catch((err2) => {
+      console.log('Signup system offline');
+    });
   }).catch((err) => {
     console.log('Signup system offline');
   });
 
-  createUser().then((result) => {
-    process.stdout.write(`${result.firstName} ${result.lastName}`);
-  }).catch((err) => {
-    console.log('Signup system offline');
-  });
+
 
 }
