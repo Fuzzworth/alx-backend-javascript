97cdf1c43b9df3c4344282c80935433656298994
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-07 16:04:56 +0200

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
-import uploadPhoto, createUser from "./utils.js";
+import { uploadPhoto, createUser } from "./utils.js";
 export default function handleProfileSignup(){
   return Promise.all([uploadPhoto(), createUser()])
     .then(([resultUP, resultCU]) => {
