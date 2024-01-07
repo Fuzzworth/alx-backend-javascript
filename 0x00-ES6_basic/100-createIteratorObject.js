5efbf280980e6afd89664699eb582b7bcbf896a5
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-07 11:34:58 +0200

    task 100

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function createIteratorObject(report) {
+  let emp = [];
+for (const [department, employees] of Object.entries(report)) {
+  for (const [key, value] of Object.entries(employees)){
+    for (let b of value){
+    emp.push(b);
+    }
+  }
+}
+return emp[Symbol.iterator]();
+}
