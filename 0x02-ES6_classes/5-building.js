735f9cbb2c93b333a18e9393624a2d39ffdc26a1
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-09 12:01:03 +0200

    task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number')
+      throw new TypeError('SQFT must be a number');
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSQFT) {
+    if (typeof newSQFT !== 'number')
+      throw new TypeError('SQFT must be a number');
+    this._sqft = newSQFT;
+  }
+  
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
