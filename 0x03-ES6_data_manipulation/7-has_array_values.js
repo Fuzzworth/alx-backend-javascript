2228de3cd43d3420d3f71ffe448a6dff1215413a
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-09 23:10:41 +0200

    task 7

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,10 @@
+export default function hasValuesFromArray(set, array){
+  if (set instanceof Set && Array.isArray(array)) {
+    const initial = true;
+    const value = array.reduce((previous, current) => previous && set.has(current), initial);
+
+    return value;
+  } else {
+    return false;
+  }
+}
