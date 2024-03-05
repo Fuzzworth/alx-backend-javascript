4e947159862c359adb99cb0d1b3d27541da57fbb
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-03-05 12:31:10 +0200

    Task 2 FIX: encoding

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,51 +1,49 @@
 const fs = require('fs');
 
-function csvJSON(csv){
+function csvJSON(csv) {
+  let numberOfStudents = 0;
 
-	let numberOfStudents = 0;
+  const lines = csv.split('\n');
 
-	let lines=csv.split("\n");
+  const result = [];
+  const fields = {};
 
-	let result = [];
-	let fields = {};
+  const headers = lines[0].split(',');
 
-	let headers=lines[0].split(",");
+  for (let i = 1; i < lines.length; i++) {
+    if (lines[i]) {
+      const obj = {};
+      const currentline = lines[i].split(',');
 
-	for(let i=1;i<lines.length;i++){
+      for (let j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentline[j];
+        if (j == 3 && currentline[j] in fields) {
+          fields[currentline[j]].push(currentline[0]);
+        } else if (j == 3 && !(currentline[j] in fields)) {
+          fields[currentline[j]] = [currentline[0]];
+        }
+      }
+      result.push(obj);
+      numberOfStudents += 1;
+    }
+  }
 
-		if (lines[i]) {
-			let obj = {};
-			let currentline=lines[i].split(",");
-
-			for(let j=0;j<headers.length;j++){
-				obj[headers[j]] = currentline[j];
-				if (j == 3 && currentline[j] in fields) {
-					fields[currentline[j]].push(currentline[0]);
-				} else if(j == 3 && !(currentline[j] in fields)) {
-					fields[currentline[j]] = [currentline[0]];
-				}
-			}
-			result.push(obj);
-			numberOfStudents++;
-		}
-	}
-
-	console.log(`Number of students: ${numberOfStudents}`);
-	for (const [key, value] of Object.entries(fields)) {
-		const numberOfStudents = value.length;
-		const listOfNames = value.join(", ");
-		console.log(`Number of students in ${key}: ${numberOfStudents}. List: ${listOfNames}`);
-	}
+  console.log(`Number of students: ${numberOfStudents}`);
+  for (const [key, value] of Object.entries(fields)) {
+    const numberOfStudents = value.length;
+    const listOfNames = value.join(', ');
+    console.log(`Number of students in ${key}: ${numberOfStudents}. List: ${listOfNames}`);
+  }
 }
 
-function countStudents(path){
-	fs.readFile(path, 'utf8', function (err, data) {
-		if (err) {
-			throw new Error("Cannot load the database");
-		}
-		let numberOfStudents = 0;
-		csvJSON(data);
-	});
+function countStudents(path) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    const numberOfStudents = 0;
+    csvJSON(data);
+  });
 }
 
 module.exports = countStudents;
