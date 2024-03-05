4b583ae102d210e88afb3439f09f3ff87989e23c
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-03-05 11:36:24 +0200

    Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,14 @@
-process.stdin.setEncoding('utf8');
+process.stdin.setEncoding("utf8");
 
-process.stdin.on('readable', function() {
-  var chunk = process.stdin.read();
+console.log("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on("readable", function() {
+  const chunk = process.stdin.read();
   if (chunk !== null) {
-    process.stdout.write('data: ' + chunk);
+    process.stdout.write("Your name is: " + chunk);
   }
 });
 
-process.stdin.on('end', function() {
-  process.stdout.write('end');
+process.stdin.on("end", function() {
+  process.stdout.write("This important software is now closing\n");
 });
